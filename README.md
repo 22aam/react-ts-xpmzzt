50c36fb79a4cf919450bb20e6a5535c313d59fe9
22aam <aschool.alden@gmail.com>
2022-05-16 12:04:01 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-xpmzzt
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-xpmzzt)
---
+++ index.tsx
@@ -0,0 +1,204 @@
+< HTMLCollection>
+<head className="log pi int LOOP"><head className="log pi int loop"></head></head>
+<head className="log pi int loop"></head>
+<head className="log pi it ll"></head>
+<_output></_output>
+<INPUT-OUTPT></INPUT-OUTPT>
+<OPEN-OUTPUT></OPEN-OUTPUT>
+<DATA>
+<LOG.PI.INT.LOOP></LOG.PI.INT.LOOP<
+  HTMLHeadElement.LOG.I.INT.DATA
+  True or True      # Evaluates to True
+True or False     # Evaluates to True
+False or False    # Evaluates to False
+1 < 2 or 3 < 1    # Evaluates to True
+3 < 1 or 1 > 6    # Evaluates to False
+1 == 1 or 1 < 2   # Evaluates to True
+is_true = True
+is_false = False
+print(type(is_true)) 
+# will output: <class 'bool'>
+  <HEADER.LOG.P.INT.LOOP></HEADER.LOG.P.INT.LOOP>
+  variable(LIST)=POWERMAINBOX
+  <HEADER.LOG.PI.INT.LOOP></HEADER.LOG.PI.INT.LOOP>
+  <HEADER.LOG.P.INT.LOOP.PI></HEADER.LOG.P.INT.LOOP.PI>
+  $array_of_arrays = ["a", "Not that lucky", 2, ["smoke detectors", "d", ["boring street in", "U.S."], "nothing happens"], "."];
+echo $array_of_arrays[3][2][1] //Returns "U.S."
+$array_of_arrays[3][2][1] = "America"; // "U.S." changed to "America"
+echo $array_of_arrays[3][2][1] //Returns "America"
+//Note - the arrays do not have to always be labelled "$pieces" & "$glue"
+// The or Operator:
+TRUE or TRUE;   // Evaluates to: TRUE
+FALSE or TRUE;  // Evaluates to: TRUE
+TRUE or FALSE;  // Evaluates to: TRUE
+FALSE or FALSE; // Evaluates to: FALSE
+// The and Operator:
+TRUE and TRUE;   // Evaluates to: TRUE
+FALSE and TRUE;  // Evaluates to: FALSE
+TRUE and FALSE;  // Evaluates to: FALSE
+FALSE and FALSE; // Evaluates to: FALSE
+eadable.
+// We can use the break statement to end the loop once the count reaches 4
+$count = 1;
+while ($count < 10)
+  echo "The count is: " . $count . "\n";
+  if ($count === 4) {
+    break;
+  $count += 1;
+  echo filter_var("<p>u</p>pies@codecademy.com", FILTER_SANITIZE_EMAIL);
+  # prints "puppies@codecademy.com"
+  echo filter_var("<p>u</p>pies@codecademy.com", FILTER_VALIDATE_EMAIL);
+  # returns false and prints nothing
+header.log.pi.int.loop< HTMLCollection>
+<head className="log pi int LOOP"><head className="log pi int loop"></head></head>
+<head className="log pi int loop"></head>
+<head className="log pi it ll"></head>
+<_output></_output>
+<INPUT-OUTPT></INPUT-OUTPT>
+<OPEN-OUTPUT></OPEN-OUTPUT>
+<DATA>
+<LOG.PI.INT.LOOP></LOG.PI.INT.LOOP<
+  HTMLHeadElement.LOG.I.INT.DATA
+  True or True      # Evaluates to True
+True or False     # Evaluates to True
+False or False    # Evaluates to False
+1 < 2 or 3 < 1    # Evaluates to True
+3 < 1 or 1 > 6    # Evaluates to False
+1 == 1 or 1 < 2   # Evaluates to True
+is_true = True
+is_false = False
+print(type(is_true)) 
+# will output: <class 'bool'>
+  <HEADER.LOG.P.INT.LOOP></HEADER.LOG.P.INT.LOOP>
+  variable(LIST)=POWERMAINBOX
+  <HEADER.LOG.PI.INT.LOOP></HEADER.LOG.PI.INT.LOOP>
+  <HEADER.LOG.P.INT.LOOP.PI></HEADER.LOG.P.INT.LOOP.PI>
+  $array_of_arrays = ["a", "Not that lucky", 2, ["smoke detectors", "d", ["boring street in", "U.S."], "nothing happens"], "."];
+echo $array_of_arrays[3][2][1] //Returns "U.S."
+$array_of_arrays[3][2][1] = "America"; // "U.S." changed to "America"
+echo $array_of_arrays[3][2][1] //Returns "America"
+//Note - the arrays do not have to always be labelled "$pieces" & "$glue"
+// The or Operator:
+TRUE or TRUE;   // Evaluates to: TRUE
+FALSE or TRUE;  // Evaluates to: TRUE
+TRUE or FALSE;  // Evaluates to: TRUE
+FALSE or FALSE; // Evaluates to: FALSE
+ 
+// The and Operator:
+TRUE and TRUE;   // Evaluates to: TRUE
+FALSE and TRUE;  // Evaluates to: FALSE
+TRUE and FALSE;  // Evaluates to: FALSE
+FALSE and FALSE; // Evaluates to: FALSE
+eadable.
+// We can use the break statement to end the loop once the count reaches 4
+$count = 1;
+while ($count < 10)
+  echo "The count is: " . $count . "\n";
+  if ($count === 4) {
+    break;
+  $count += 1;
+  echo filter_var("<p>u</p>pies@codecademy.com", FILTER_SANITIZE_EMAIL);
+  # prints "puppies@codecademy.com"
+  echo filter_var("<p>u</p>pies@codecademy.com", FILTER_VALIDATE_EMAIL);
+  # returns false and prints nothing
+  < HTMLCollection>
+<head className="log pi int LOOP"><head className="log pi int loop"></head></head>
+<head className="log pi int loop"></head>
+<head className="log pi it ll"></head>
+<_output></_output>
+<INPUT-OUTPT></INPUT-OUTPT>
+<OPEN-OUTPUT></OPEN-OUTPUT>
+<DATA>
+<LOG.PI.INT.LOOP></LOG.PI.INT.LOOP<
+  HTMLHeadElement.LOG.I.INT.DATA
+  True or True      # Evaluates to True
+True or False     # Evaluates to True
+False or False    # Evaluates to False
+1 < 2 or 3 < 1    # Evaluates to True
+3 < 1 or 1 > 6    # Evaluates to False
+1 == 1 or 1 < 2   # Evaluates to True
+is_true = True
+is_false = False
+print(type(is_true)) 
+# will output: <class 'bool'>
+  <HEADER.LOG.P.INT.LOOP></HEADER.LOG.P.INT.LOOP>
+  variable(LIST)=POWERMAINBOX
+  <HEADER.LOG.PI.INT.LOOP></HEADER.LOG.PI.INT.LOOP>
+  <HEADER.LOG.P.INT.LOOP.PI></HEADER.LOG.P.INT.LOOP.PI>
+  $array_of_arrays = ["a", "Not that lucky", 2, ["smoke detectors", "d", ["boring street in", "U.S."], "nothing happens"], "."];
+echo $array_of_arrays[3][2][1] //Returns "U.S."
+$array_of_arrays[3][2][1] = "America"; // "U.S." changed to "America"
+echo $array_of_arrays[3][2][1] //Returns "America"
+//Note - the arrays do not have to always be labelled "$pieces" & "$glue"
+// The or Operator:
+TRUE or TRUE;   // Evaluates to: TRUE
+FALSE or TRUE;  // Evaluates to: TRUE
+TRUE or FALSE;  // Evaluates to: TRUE
+FALSE or FALSE; // Evaluates to: FALSE
+// The and Operator:
+TRUE and TRUE;   // Evaluates to: TRUE
+FALSE and TRUE;  // Evaluates to: FALSE
+TRUE and FALSE;  // Evaluates to: FALSE
+FALSE and FALSE; // Evaluates to: FALSE
+eadable.
+// We can use the break statement to end the loop once the count reaches 4
+$count = 1;
+while ($count < 10)
+  echo "The count is: " . $count . "\n";
+  if ($count === 4) {
+    break;
+  $count += 1;
+  echo filter_var("<p>u</p>pies@codecademy.com", FILTER_SANITIZE_EMAIL);
+  # prints "puppies@codecademy.com"
+  echo filter_var("<p>u</p>pies@codecademy.com", FILTER_VALIDATE_EMAIL);
+  # returns false and prints nothing
+header.log.pi.int.loop< HTMLCollection>
+<head className="log pi int LOOP"><head className="log pi int loop"></head></head>
+<head className="log pi int loop"></head>
+<head className="log pi it ll"></head>
+<_output></_output>
+<INPUT-OUTPT></INPUT-OUTPT>
+<OPEN-OUTPUT></OPEN-OUTPUT>
+<DATA>
+<LOG.PI.INT.LOOP></LOG.PI.INT.LOOP<
+  HTMLHeadElement.LOG.I.INT.DATA
+  True or True      # Evaluates to True
+True or False     # Evaluates to True
+False or False    # Evaluates to False
+1 < 2 or 3 < 1    # Evaluates to True
+3 < 1 or 1 > 6    # Evaluates to False
+1 == 1 or 1 < 2   # Evaluates to True
+is_true = True
+is_false = False
+print(type(is_true)) 
+# will output: <class 'bool'>
+  <HEADER.LOG.P.INT.LOOP></HEADER.LOG.P.INT.LOOP>
+  variable(LIST)=POWERMAINBOX
+  <HEADER.LOG.PI.INT.LOOP></HEADER.LOG.PI.INT.LOOP>
+  <HEADER.LOG.P.INT.LOOP.PI></HEADER.LOG.P.INT.LOOP.PI>
+  $array_of_arrays = ["a", "Not that lucky", 2, ["smoke detectors", "d", ["boring street in", "U.S."], "nothing happens"], "."];
+echo $array_of_arrays[3][2][1] //Returns "U.S."
+$array_of_arrays[3][2][1] = "America"; // "U.S." changed to "America"
+echo $array_of_arrays[3][2][1] //Returns "America"
+//Note - the arrays do not have to always be labelled "$pieces" & "$glue"
+// The or Operator:
+TRUE or TRUE;   // Evaluates to: TRUE
+FALSE or TRUE;  // Evaluates to: TRUE
+TRUE or FALSE;  // Evaluates to: TRUE
+FALSE or FALSE; // Evaluates to: FALSE
+ 
+// The and Operator:
+TRUE and TRUE;   // Evaluates to: TRUE
+FALSE and TRUE;  // Evaluates to: FALSE
+TRUE and FALSE;  // Evaluates to: FALSE
+FALSE and FALSE; // Evaluates to: FALSE
+eadable.
+// We can use the break statement to end the loop once the count reaches 4
+$count = 1;
+while ($count < 10)
+  echo "The count is: " . $count . "\n";
+  if ($count === 4) {
+    break;
+  $count += 1;
+  # returns false and prints nothing
+  HEADER.log.pi,int.loop
